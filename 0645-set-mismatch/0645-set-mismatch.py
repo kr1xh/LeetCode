cdbc034f867aacd9a6a4a8e8f65f4c659dfd5a7d
Krish <krishchandpurkar20@gmail.com>
2026-02-16 00:31:14 +0530

    Time: 8 ms (76.18%), Space: 20.6 MB (49.88%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        duplicate = -1
+        missing = -1
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] < 0:
+                duplicate = abs(num)
+            else:
+                nums[index] *= -1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                missing = i + 1
+                break
+        return [duplicate, missing]
