16e12dab7555554a37d74fae15630c8fb450d766
Krish <krishchandpurkar20@gmail.com>
2026-02-25 00:12:45 +0530

    Time: 273 ms (79.89%), Space: 67 MB (78%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    counts = orders["customer_number"].value_counts()
+    
+    top_customer = counts.idxmax()
+    
+    return pd.DataFrame({"customer_number": [top_customer]})
