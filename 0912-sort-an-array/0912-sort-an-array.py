2c20d4e1d4ca9602c9c0e93b24a36eba2c8eb63c
Krish <krishchandpurkar20@gmail.com>
2026-03-02 00:01:59 +0530

    Time: 984 ms (6.66%), Space: 38.1 MB (6.12%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+
+        def heapify(n, i):
+            largest = i
+            left = 2 * i + 1
+            right = 2 * i + 2
+
+            if left < n and nums[left] > nums[largest]:
+                largest = left
+
+            if right < n and nums[right] > nums[largest]:
+                largest = right
+
+            if largest != i:
+                nums[i], nums[largest] = nums[largest], nums[i]
+                heapify(n, largest)
+
+        for i in range(n // 2 - 1, -1, -1):
+            heapify(n, i)
+
+        for i in range(n - 1, 0, -1):
+            nums[0], nums[i] = nums[i], nums[0]
+            heapify(i, 0)
+
+        return nums
