76a7751b2597f0f3e6a97c5307f9e59c8214f554
Krish <krishchandpurkar20@gmail.com>
2026-03-03 19:39:41 +0530

    Time: 1 ms (52.32%), Space: 19.4 MB (47.04%) - LeetHub

---
+++ 0932-beautiful-array/0932-beautiful-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def beautifulArray(self, n: int) -> list[int]:
+        res = [1]
+        
+        while len(res) < n:
+            temp = []
+            
+            for x in res:
+                if 2 * x - 1 <= n:
+                    temp.append(2 * x - 1)
+            
+            for x in res:
+                if 2 * x <= n:
+                    temp.append(2 * x)
+            
+            res = temp
+        
+        return res
