2565857b2cd57aedf8947f419ce7f32c576b2a5c
Krish <krishchandpurkar20@gmail.com>
2026-03-09 23:30:23 +0530

    Time: 356 ms (5.19%), Space: 67.6 MB (68.28%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    
+    result = (
+        actor_director
+        .groupby(['actor_id', 'director_id'])
+        .size()
+        .reset_index(name='count')
+    )
+    
+    return result[result['count'] >= 3][['actor_id', 'director_id']]
