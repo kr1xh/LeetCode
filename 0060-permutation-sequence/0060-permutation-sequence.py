58f667864575c6e07ed14689ba5ef314a3c4726b
Krish <krishchandpurkar20@gmail.com>
2026-03-25 20:52:09 +0530

    Time: 0 ms (100%), Space: 19.5 MB (57.74%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        import math
+        
+        nums = [str(i) for i in range(1, n + 1)]
+        k -= 1
+        
+        res = []
+        
+        for i in range(n, 0, -1):
+            fact = math.factorial(i - 1)
+            index = k // fact
+            
+            res.append(nums[index])
+            nums.pop(index)
+            
+            k %= fact
+        
+        return "".join(res)
