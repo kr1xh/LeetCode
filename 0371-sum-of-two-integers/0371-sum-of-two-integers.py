22be41e73b5377604f0a4581cf465ce3e3d81c3c
Krish <krishchandpurkar20@gmail.com>
2026-04-03 23:17:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (35.61%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        max_int = 0x7FFFFFFF
+        
+        while b != 0:
+            temp = (a ^ b) & mask
+            
+            b = ((a & b) << 1) & mask
+            
+            a = temp
+        
+        return a if a <= max_int else ~(a ^ mask)
